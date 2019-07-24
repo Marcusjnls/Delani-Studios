0810b35933c682ae95ab2fba36274ef1a345234e
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-07-24 08:24:38 +0300

    Add hover text style on portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -24,46 +24,46 @@ $(document).ready(function() {
     $(".product-text").hide();
   })
 });
-// //hover effect
-// $(document).ready(function() {
-//   $('.wrap1').mouseover(function() {
-//     $('.hovertext1').show();
-//   }).mouseout(function() {
-//     $('.hovertext1').hide();
-//   });
-//   $('.wrap2').mouseover(function() {
-//     $('.hovertext2').show();
-//   }).mouseout(function() {
-//     $('.hovertext2').hide();
-//   });
-//   $('.wrap3').mouseover(function() {
-//     $('.hovertext3').show();
-//   }).mouseout(function() {
-//     $('.hovertext3').hide();
-//   });
-//   $('.wrap4').mouseover(function() {
-//     $('.hovertext4').show();
-//   }).mouseout(function() {
-//     $('.hovertext4').hide();
-//   });
-//   $('.wrap5').mouseover(function() {
-//     $('.hovertext5').show();
-//   }).mouseout(function() {
-//     $('.hovertext5').hide();
-//   });
-//   $('.wrap6').mouseover(function() {
-//     $('.hovertext6').show();
-//   }).mouseout(function() {
-//     $('.hovertext6').hide();
-//   });
-//   $('.wrap7').mouseover(function() {
-//     $('.hovertext7').show();
-//   }).mouseout(function() {
-//     $('.hovertext7').hide();
-//   });
-//   $('.wrap8').mouseover(function() {
-//     $('.hovertext8').show();
-//   }).mouseout(function() {
-//     $('.hovertext8').hide();
-//   });
-// });
+//hover effect
+$(document).ready(function() {
+  $('.wrap1').mouseover(function() {
+    $('.hovertext1').show();
+  }).mouseout(function() {
+    $('.hovertext1').hide();
+  });
+  $('.wrap2').mouseover(function() {
+    $('.hovertext2').show();
+  }).mouseout(function() {
+    $('.hovertext2').hide();
+  });
+  $('.wrap3').mouseover(function() {
+    $('.hovertext3').show();
+  }).mouseout(function() {
+    $('.hovertext3').hide();
+  });
+  $('.wrap4').mouseover(function() {
+    $('.hovertext4').show();
+  }).mouseout(function() {
+    $('.hovertext4').hide();
+  });
+  $('.wrap5').mouseover(function() {
+    $('.hovertext5').show();
+  }).mouseout(function() {
+    $('.hovertext5').hide();
+  });
+  $('.wrap6').mouseover(function() {
+    $('.hovertext6').show();
+  }).mouseout(function() {
+    $('.hovertext6').hide();
+  });
+  $('.wrap7').mouseover(function() {
+    $('.hovertext7').show();
+  }).mouseout(function() {
+    $('.hovertext7').hide();
+  });
+  $('.wrap8').mouseover(function() {
+    $('.hovertext8').show();
+  }).mouseout(function() {
+    $('.hovertext8').hide();
+  });
+});
