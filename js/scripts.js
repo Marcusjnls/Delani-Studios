0d5b0eaffffc281c633f86d968bac520541dda8e
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-07-24 08:22:01 +0300

    Add toggle function to icons on what we do section

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+  $("#design-pic").click(function() {
+    $(".design-text").toggle();
+    $("#design-pic").toggle();
+  });
+  $(".design-text").click(function() {
+    $("#design-pic").show();
+    $(".design-text").hide();
+  })
+  $("#development-pic").click(function() {
+    $(".development-text").toggle();
+    $("#development-pic").toggle();
+  });
+  $(".development-text").click(function() {
+    $("#development-pic").show();
+    $(".development-text").hide();
+  })
+  $("#product-pic").click(function() {
+    $(".product-text").toggle();
+    $("#product-pic").toggle();
+  });
+  $(".product-text").click(function() {
+    $("#product-pic").show();
+    $(".product-text").hide();
+  })
+});
+// //hover effect
+// $(document).ready(function() {
+//   $('.wrap1').mouseover(function() {
+//     $('.hovertext1').show();
+//   }).mouseout(function() {
+//     $('.hovertext1').hide();
+//   });
+//   $('.wrap2').mouseover(function() {
+//     $('.hovertext2').show();
+//   }).mouseout(function() {
+//     $('.hovertext2').hide();
+//   });
+//   $('.wrap3').mouseover(function() {
+//     $('.hovertext3').show();
+//   }).mouseout(function() {
+//     $('.hovertext3').hide();
+//   });
+//   $('.wrap4').mouseover(function() {
+//     $('.hovertext4').show();
+//   }).mouseout(function() {
+//     $('.hovertext4').hide();
+//   });
+//   $('.wrap5').mouseover(function() {
+//     $('.hovertext5').show();
+//   }).mouseout(function() {
+//     $('.hovertext5').hide();
+//   });
+//   $('.wrap6').mouseover(function() {
+//     $('.hovertext6').show();
+//   }).mouseout(function() {
+//     $('.hovertext6').hide();
+//   });
+//   $('.wrap7').mouseover(function() {
+//     $('.hovertext7').show();
+//   }).mouseout(function() {
+//     $('.hovertext7').hide();
+//   });
+//   $('.wrap8').mouseover(function() {
+//     $('.hovertext8').show();
+//   }).mouseout(function() {
+//     $('.hovertext8').hide();
+//   });
+// });
